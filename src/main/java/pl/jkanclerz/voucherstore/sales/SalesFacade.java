f9e8e767e774b744b7a26f18ab17faada68c066a
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2020-12-01 12:51:43 +0100

    instroduce basket unit

---
+++ src/main/java/pl/jkanclerz/voucherstore/sales/SalesFacade.java
@@ -0,0 +1,5 @@
+package pl.jkanclerz.voucherstore.sales;
+
+public class SalesFacade {
+
+}
---
+++ src/test/java/pl/jkanclerz/voucherstore/sales/SalesTest.java
@@ -0,0 +1,20 @@
+package pl.jkanclerz.voucherstore.sales;
+
+import org.junit.Test;
+
+public class SalesTest {
+
+    @Test
+    public void itAllowAddProductToBasket()
+    {
+        //Arrange
+//        SalesFacade sales = thereIsSalesModule();
+//        String productId = thereIsProductAvailable();
+
+        //Act
+//        sales.addToBusket(productId);
+
+        //Assert
+
+    }
+}
